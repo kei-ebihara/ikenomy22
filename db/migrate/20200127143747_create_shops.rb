cddcf6f5b657b6c48f64b46ad606be2ef1659edd
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2020-01-27 23:41:39 +0900

    add Post Model

---
+++ db/migrate/20200127143747_create_shops.rb
@@ -0,0 +1,15 @@
+class CreateShops < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shops do |t|
+      t.string :name, null: false, length: 50
+      t.text :description, null: false, length: 1000
+      t.string :phone_number, length: 15
+      t.string :address, length: 100
+      t.text :homepage, length: 1000
+      t.text :business_hours, length: 100
+      t.string :holiday, length: 50
+
+      t.timestamps
+    end
+  end
+end
