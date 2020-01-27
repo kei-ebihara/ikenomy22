7a408a52d9b5850688524c57a803df3b682d7e53
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2020-01-28 00:09:19 +0900

    shop登録機能作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,4 +2,21 @@ class ShopsController < ApplicationController
   def index
     @shops = Shop.all
   end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    @shop.save
+    redirect_to @shop, notice: "お店を登録しました。"
+  end
+  
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :description, :phone_number, :address, :homepage, :business_hours, :holiday)
+  end
+
 end
